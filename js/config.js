2bd9567d53815e8f8693ea56425e89ee90c97025
smkn2lebong <115885509+smkn2lebong@users.noreply.github.com>
2025-10-23 01:51:33 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,9 +4,9 @@ const CONFIG = {
     PASSWORD_PIKET: "piket2024",
     PASSWORD_KEPSEK: "kepsek2024",
     
-    // URLs untuk data
-    DATA_SISWA_URL: '../data/siswa.json',
-    DATA_KELAS_URL: '../data/kelas.json',
+    // URLs untuk data - PATH ABSOLUT untuk server sekolah
+    DATA_SISWA_URL: 'https://smkn2lebong.sch.id/hadir/data/siswa.json',
+    DATA_KELAS_URL: 'https://smkn2lebong.sch.id/hadir/data/kelas.json',
     
     // Google Apps Script URL
     WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxaJlsavuEkPzCjQDll33Y7Mx7tazRiuk0jxIuO6jzKR0O5ksEHOAwJU-Zi0MOSRoxs/exec'
